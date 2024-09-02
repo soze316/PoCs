cad0c77576598ebb4c15eeedd37bc63ebc3402e6
soze <74239824+soze316@users.noreply.github.com>
2024-09-02 22:14:21 +0100

    Update apps.py

--- apps.py
+++ apps.py
@@ -6,7 +6,7 @@ def main():
     st.title("Welcome to Our POC Projects")
     st.write("Explore our innovative proof of concept projects selected for this AI workshop.:")
 
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns(2)
 
     with col1:
         st.subheader("Say What You Can See")
@@ -15,80 +15,59 @@ def main():
         st.markdown("[Play a simple game!](https://artsandculture.google.com/experiment/say-what-you-see/jwG3m7wQShZngw?hl=en)", unsafe_allow_html=True)
 
     with col2:
-        st.subheader("Ghostwriter Basic")
-        st.image("poc_1.png", width=150)
-        st.write("AI-powered author services for various content types to help initiate ideas.")
-        st.markdown("[Explore Project](https://hpg-ghostwriter-basic.streamlit.app/)", unsafe_allow_html=True)
-
-    with col3:
-        st.subheader("Ghostwriter Advanced")
+        st.subheader("Ghostwriter")
         st.image("poc_2.png", width=150)
-        st.write("AI-powered author services with more options to steer the book generation for various content types to help initiate ideas.")
+        st.write("AI-powered author services with more options to steer the book generation for various content types to help initiate ideas")
         st.markdown("[Explore Project](https://hpg-ghostwriter-basic.streamlit.app/advanced)", unsafe_allow_html=True)
+
+    #with col3:
+        #st.subheader("Ghostwriter Advanced")
+        #st.image("poc_2.png", width=150)
+        #st.write("AI-powered author services with more options to steer the book generation for various content types to help initiate ideas.")
+        #st.markdown("[Explore Project](https://hpg-ghostwriter-basic.streamlit.app/advanced)", unsafe_allow_html=True)
 
     # Add some space between rows
     st.write("")
     st.write("")
 
-    col4, col5, col6 = st.columns(3)
+    col3, col4 = st.columns(2)
     
-    with col4:
+    with col3:
         st.subheader("Notebook LM")
         st.image("notebook.png", width=150)
         st.write("Interact with any of your internal documents. Internal HR documents, travel policies, legal contracts, etc.")
         st.markdown("[Private](https://notebooklm.google.com/notebook/7befb094-a5a2-43f7-87cf-8d22b4ea1c99)", unsafe_allow_html=True)
         st.markdown("[Corporate](https://notebooklm.google.com/notebook/b5427c43-cdc8-4703-85f9-b0a4e6f20e78)", unsafe_allow_html=True)
 
-    with col5:
-        st.subheader("Hey Gen")
-        st.image("hey_gen.png", width=150)
-        st.write("Create a custom avatar of yourself train your voice to generate custom content. (Hardware provided)")
-        st.markdown("[Create your own custom avatar!](https://www.heygen.com/)", unsafe_allow_html=True)
-
-    with col6:
-        st.subheader("Talk with Data")
-        st.image("https://via.placeholder.com/150", width=150)
-        st.write("Interact with your data through natural language queries.")
-        st.markdown("[Explore Project](https://external-url-for-project1.com)", unsafe_allow_html=True)
+    with col4:
+        st.subheader("Podcasts")
+        st.image("illuminate.png", width=150)
+        st.write("Generate short podcasts with your published content.")
+        st.markdown("[Listen to summary  AI podcasts](https://illuminate.google.com/home?pli=1)", unsafe_allow_html=True)
 
     # Add some space between rows
     st.write("")
     st.write("")
 
     # Second row
-    col7, col8, col9 = st.columns(3)
+    col5, col6 = st.columns(2)
 
-
-    with col7:
-        st.subheader("Illuminate")
-        st.image("illuminate.png", width=150)
-        st.write("Generate short podcasts with your published content.")
-        st.markdown("[Listen to summary  AI podcasts](https://illuminate.google.com/home?pli=1)", unsafe_allow_html=True)
-
-    with col8:
+    with col5:
         st.subheader("Build your own app")
         st.image("https://via.placeholder.com/150", width=150)
         st.write("Use AI to quickly build small custom applications for your teams to solve small, very specific problems.")
         st.markdown("[Explore Project](https://claude.ai/)", unsafe_allow_html=True)
 
-    with col9:
-        st.subheader("Novel Science Ideas")
-        st.image("https://via.placeholder.com/150", width=150)
-        st.write("Test the novelty of your scientific ideas and generate new ones.")
-        st.markdown("[Explore Project](project9.py)", unsafe_allow_html=True)
+    with col6:
+        st.subheader("Hey Gen")
+        st.image("hey_gen.png", width=150)
+        st.write("Create a custom avatar of yourself train your voice to generate custom content. (Hardware provided)")
+        st.markdown("[Create your own custom avatar!](https://www.heygen.com/)", unsafe_allow_html=True)
 
     # Add some space between rows
     st.write("")
     st.write("")
 
-    # Second row
-    col10 = st.columns(3)
-
-    with col10:
-        st.subheader("Oxford English Dictionary")
-        st.image("oxford.png", width=150)
-        st.write("Automate the process of using the correct words for your region in your manuscripts. Built using artefacts.")
-        st.markdown("[Explore Project](https://oed-basic.streamlit.app/)", unsafe_allow_html=True)
 
     st.subheader("Additional Tools", divider=True)
 
